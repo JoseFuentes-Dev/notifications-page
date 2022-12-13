2e6f8ac549e7acc7b3a32f775012933db58748c6
JoseFuentes-Dev <jocfuentes94@gmail.com>
2022-12-13 12:33:51 -0500

    js complete

--- main.js
+++ main.js
@@ -2,13 +2,22 @@
 let notification = document.querySelector('.noti-span');
 //Mark all as read
 let mark = document.querySelector(".mark");
+let noti = document.querySelectorAll('.notify');
 mark.addEventListener("click", removeActive);
 function removeActive() {
-    let noti = document.querySelectorAll('.notify');
     noti.forEach(element => {
         element.classList.remove("active");
         notification.innerHTML = `0`;
     });
   }
 // Individual Read
-
+let notspan = notification.innerHTML;
+let chan = parseInt(notspan);
+for (const i of noti) {
+    i.addEventListener("click",()=>{
+        if(i.classList[1]=='active'){
+            i.classList.remove('active');       
+            notification.innerHTML =--chan;
+        }
+    }); 
+}
